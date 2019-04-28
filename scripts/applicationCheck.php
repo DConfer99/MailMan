1333b922748de1c6c9d9c192083f6e14951ecf0f
Brennan Doherty <dohertybrennan@yahoo.com>
2019-04-28 14:11:46 -0400

    Added Postfix Checks
    Added script to check if the postfix application is installed and the main.cf config file

--- scripts/applicationCheck.php
+++ scripts/applicationCheck.php
@@ -9,6 +9,27 @@ if ($postfix_check == "") {
     $err->displayError("Postfix is not installed on this server. Please install using the command: \n <i>DEBIAN_FRONTEND=noninteractive apt-get install postfix</i> ");
 }
 
+//Looks to see the exsistance and permissions of /etc/postfix/main.cf
+$postfix_config_check = shell_exec("ls -l /etc/postfix/main.cf");
+
+//Halts operationif /etc/postfix/main.cf does not exsist
+if ($postfix_config_check == "") {
+    $err = new fatalError;
+    $err->displayError("Postfix config file does not exsist. Please execute the command: <i>sudo touch /etc/postfix/main.cf</i>");
+}
+
+//Looks to see if the correct permissions are set on the file
+if (!fnmatch("-rw-r--r--*", $postfix_config_check)) {
+    $err = new fatalError;
+    $err->displayError("Permissions on /etc/postfix/main.cf are not correct. Please change them by running: <i>sudo chmod 622 /etc/postfix/main.cf</i>");
+}
+
+//Looks to see if www-data is the owner of /etc/postfix/main.cf
+if (!fnmatch("*www-data root*", $postfix_config_check)) {
+    $err = new fatalError;
+    $err->displayError("www-data is not the owner of /etc/postfix/main.cf. Please change it by running: <i>sudo chown www-data:root /etc/postfix/main.cf</i>");
+}
+
 //TODO:
-//!: Add Dovecot Support!!!
+//1: Add Dovecot Support!!!
 ?>
